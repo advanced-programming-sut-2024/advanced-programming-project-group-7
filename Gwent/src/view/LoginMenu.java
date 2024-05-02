496851d2a3a373787959eb4143085963de07bc84
mohammad ebrahimian <s.mohammad.e.1383@gmail.com>
2024-05-02 16:14:50 -0700

    login menu and controller

--- Gwent/src/view/LoginMenu.java
+++ Gwent/src/view/LoginMenu.java
@@ -1,4 +1,24 @@
 package view;
 
+<<<<<<< Updated upstream
 public class LoginMenu {
+=======
+import controller.LoginMenuController;
+import enums.LoginMenuCommands;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+
+public class LoginMenu {
+    public static void run(Scanner scanner){
+        String inputCommand=scanner.nextLine();
+        if(LoginMenuCommands.registerCommand.getMatcher(inputCommand).matches()){
+            Matcher registerMatcher=LoginMenuCommands.registerCommand.getMatcher(inputCommand);
+            if(LoginMenuController.isUsernameDuplicate(registerMatcher)) System.out.println("");
+            else if(!LoginMenuController.isUsernameValid(registerMatcher)) System.out.println("");
+            else if()
+        }
+        else if()
+    }
+>>>>>>> Stashed changes
 }
