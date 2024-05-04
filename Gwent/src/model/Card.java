0f539397fad3c66106b69479feb4085000eed234
mohammad ebrahimian <s.mohammad.e.1383@gmail.com>
2024-05-03 19:38:20 -0700

    user model completed

---
+++ Gwent/src/model/Card.java
@@ -0,0 +1,7 @@
+package model;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Card {
+}
--- Gwent/src/view/LoginMenu.java
+++ Gwent/src/view/LoginMenu.java
@@ -1,14 +1,8 @@
 package view;
-
-<<<<<<< Updated upstream
-public class LoginMenu {
-=======
 import controller.LoginMenuController;
 import enums.LoginMenuCommands;
-
 import java.util.Scanner;
 import java.util.regex.Matcher;
-
 public class LoginMenu {
     public static void run(Scanner scanner){
         String inputCommand=scanner.nextLine();
@@ -16,9 +10,6 @@ public class LoginMenu {
             Matcher registerMatcher=LoginMenuCommands.registerCommand.getMatcher(inputCommand);
             if(LoginMenuController.isUsernameDuplicate(registerMatcher)) System.out.println("");
             else if(!LoginMenuController.isUsernameValid(registerMatcher)) System.out.println("");
-            else if()
         }
-        else if()
     }
->>>>>>> Stashed changes
 }
