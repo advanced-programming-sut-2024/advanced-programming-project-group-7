8cd4fb7ecea6c0acb70dc568d590caf14686801a
kooroshIM99 <kooroshimani2023@gmail.com>
2024-06-22 18:43:23 +0330

    preGame choose faction

--- Gwent/src/Gwent/src/main/java/model/NorthernRealms.java
+++ Gwent/src/Gwent/src/main/java/model/NorthernRealms.java
@@ -1,10 +1,69 @@
 package model;
 
+import java.util.ArrayList;
+
 public class NorthernRealms extends Faction{
-
     private String[] leaderNames = {""};
-
     public NorthernRealms(String descriptions) {
         super(descriptions);
+        this.factionName="realms";
+    }
+    public static ArrayList<Card> northernRealmsCards=new ArrayList<>();
+    static {
+        northernRealmsCards.add(new Horn("horn", 3 , true, 0, "special"));
+        northernRealmsCards.add(new Decoy("decoy", 3 , true, 0, "special"));
+        northernRealmsCards.add(new Mardroeme("mardroeme", 3 , true, 0, "special"));
+        northernRealmsCards.add(new Scorch("scorch", 3 , true, 0, "special"));
+        northernRealmsCards.add(new Card("frost", 3 , true, 0, "weather"));
+        northernRealmsCards.add(new Card("clear", 2 , true, 0, "weather"));
+        northernRealmsCards.add(new Card("fog", 3 , true, 0, "weather"));
+        northernRealmsCards.add(new Card("storm", 3 , true, 0, "weather"));
+        northernRealmsCards.add(new Card("rain", 2 , true, 0, "weather"));
+        northernRealmsCards.add(new Card("ciri", 1 , false, 15, "neutral"));
+        northernRealmsCards.add(new Card("geralt", 1 , false, 15, "neutral"));
+        northernRealmsCards.add(new Card("esterad", 1 , false, 10, "realms"));
+        northernRealmsCards.add(new Card("natalis", 1 , false, 10, "realms"));
+        northernRealmsCards.add(new Card("philippa", 1 , false, 10, "realms"));
+        northernRealmsCards.add(new Card("vernon", 1 , false, 10, "realms"));
+        northernRealmsCards.add(new TightBond("catapult 1", 2 , false, 8, "realms"));
+        northernRealmsCards.add(new Card("triss", 1 , false, 7, "neutral"));
+        northernRealmsCards.add(new Scorch("villen", 1 , false, 7, "neutral"));
+        northernRealmsCards.add(new Medic("yennefer", 1 , false, 7, "neutral"));
+        northernRealmsCards.add(new Card("ballista", 1 , false, 6, "realms"));
+        northernRealmsCards.add(new Card("dethmold", 1 , false, 6, "realms"));
+        northernRealmsCards.add(new MoralBoost("olgierd", 1 , false, 6, "neutral"));
+        northernRealmsCards.add(new Card("siege tower", 1 , false, 6, "realms"));
+        northernRealmsCards.add(new Card("trebuchet", 1 , false, 6, "realms"));
+        northernRealmsCards.add(new Card("trebuchet 1", 1 , false, 6, "realms"));
+        northernRealmsCards.add(new Card("vesemir", 1 , false, 6, "neutral"));
+        northernRealmsCards.add(new TightBond("crinfrid", 3 , false, 5, "realms"));
+        northernRealmsCards.add(new Medic("banner nurse", 1 , false, 5, "realms"));
+        northernRealmsCards.add(new Card("emiel", 1 , false, 5, "neutral"));
+        northernRealmsCards.add(new Card("keira", 1 , false, 5, "realms"));
+        northernRealmsCards.add(new Spy("stennis", 1 , false, 5, "realms"));
+        northernRealmsCards.add(new Card("siegfried", 1 , false, 5, "realms"));
+        northernRealmsCards.add(new Card("sheala", 1 , false, 5, "realms"));
+        northernRealmsCards.add(new Card("ves", 1 , false, 5, "realms"));
+        northernRealmsCards.add(new Card("zoltan", 1 , false, 5, "neutral"));
+        northernRealmsCards.add(new TightBond("blue stripes", 3 , false, 4, "realms"));
+        northernRealmsCards.add(new Muster("gaunter odimm darkness", 3 , false, 4, "neutral"));
+        northernRealmsCards.add(new Card("sabrina", 1 , false, 4, "realms"));
+        northernRealmsCards.add(new Card("sheldon", 1 , false, 4, "realms"));
+        northernRealmsCards.add(new Spy("dijkstra", 1 , false, 4, "realms"));
+        northernRealmsCards.add(new Horn("dandelion", 1 , false, 2, "neutral"));
+        northernRealmsCards.add(new Muster("gaunter odimm", 1 , false, 2, "neutral"));
+        northernRealmsCards.add(new Horn("yarpen", 1 , false, 2, "realms"));
+        northernRealmsCards.add(new MoralBoost("kaedwen siege", 1 , false, 1, "realms"));
+        northernRealmsCards.add(new MoralBoost("kaedwen siege 1", 1 , false, 1, "realms"));
+        northernRealmsCards.add(new MoralBoost("kaedwen siege 2", 1 , false, 1, "realms"));
+        northernRealmsCards.add(new TightBond("poor infantry", 4 , false, 1, "realms"));
+        northernRealmsCards.add(new Horn("redania", 1 , false, 1, "realms"));
+        northernRealmsCards.add(new Horn("redania 1", 1 , false, 1, "realms"));
+        northernRealmsCards.add(new Spy("thaler", 1 , false, 1, "realms"));
+        northernRealmsCards.add(new Cow("cow", 1 , false, 0, "neutral"));
+        northernRealmsCards.add(new Spy("mysterious elf", 1 , false, 0, "neutral"));
+    }
+    public String[] getLeaderNames() {
+        return leaderNames;
     }
 }
--- Gwent/src/Gwent/src/main/java/model/Scoiatael.java
+++ Gwent/src/Gwent/src/main/java/model/Scoiatael.java
@@ -1,7 +1,77 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Scoiatael extends Faction{
     public Scoiatael(String descriptions) {
         super(descriptions);
+        this.factionName="scoiatael";
+    }
+    public static ArrayList<Card> scoiataelCards = new ArrayList<>();
+    static {
+        scoiataelCards.add(new Decoy("horn", 3, true, 0, "special"));
+        scoiataelCards.add(new Horn("decoy", 3, true, 0, "special"));
+        scoiataelCards.add(new Mardroeme("mardroeme", 3, true, 0, "special"));
+        scoiataelCards.add(new Scorch("scorch", 3, true, 0, "special"));
+        scoiataelCards.add(new Card("frost", 3, true, 0, "weather"));
+        scoiataelCards.add(new Card("clear", 2, true, 0, "weather"));
+        scoiataelCards.add(new Card("fog", 3, true, 0, "weather"));
+        scoiataelCards.add(new Card("storm", 3, true, 0, "weather"));
+        scoiataelCards.add(new Card("rain", 2, true, 0, "weather"));
+        scoiataelCards.add(new Card("ciri", 1, false, 15, "neutral"));
+        scoiataelCards.add(new Card("geralt", 1, false, 15, "neutral"));
+        scoiataelCards.add(new Card("eithne", 1, false, 10, "scoiatael"));
+        scoiataelCards.add(new Card("iorveth", 1, false, 10, "scoiatael"));
+        scoiataelCards.add(new MoralBoost("isengrim", 1, false, 10, "scoiatael"));
+        scoiataelCards.add(new MoralBoost("milva", 1, false, 10, "scoiatael"));
+        scoiataelCards.add(new Card("saskia", 1, false, 10, "scoiatael"));
+        scoiataelCards.add(new Scorch("schirru", 1, false, 8, "scoiatael"));
+        scoiataelCards.add(new Card("triss", 1, false, 7, "neutral"));
+        scoiataelCards.add(new Scorch("villen", 1, false, 7, "neutral"));
+        scoiataelCards.add(new Medic("yennefer", 1, false, 7, "neutral"));
+        scoiataelCards.add(new Agile("barclay", 1, false, 6, "scoiatael"));
+        scoiataelCards.add(new Card("dennis", 1, false, 6, "scoiatael"));
+        scoiataelCards.add(new Agile("dol infantry", 1, false, 6, "scoiatael"));
+        scoiataelCards.add(new Agile("dol infantry 1", 1, false, 6, "scoiatael"));
+        scoiataelCards.add(new Agile("dol infantry 2", 1, false, 6, "scoiatael"));
+        scoiataelCards.add(new Agile("filavandrel", 1, false, 6, "scoiatael"));
+        scoiataelCards.add(new Card("ida", 1, false, 6, "scoiatael"));
+        scoiataelCards.add(new MoralBoost("olgierd", 1, false, 6, "neutral"));
+        scoiataelCards.add(new Card("vesemir", 1, false, 6, "neutral"));
+        scoiataelCards.add(new Agile("yaevinn", 1, false, 6, "scoiatael"));
+        scoiataelCards.add(new Card("emiel", 1, false, 5, "neutral"));
+        scoiataelCards.add(new Muster("havekar support", 1, false, 5, "scoiatael"));
+        scoiataelCards.add(new Muster("havekar support 1", 1, false, 5, "scoiatael"));
+        scoiataelCards.add(new Muster("havekar support 2", 1, false, 5, "scoiatael"));
+        scoiataelCards.add(new Card("mahakam", 1, false, 5, "scoiatael"));
+        scoiataelCards.add(new Card("mahakam 1", 1, false, 5, "scoiatael"));
+        scoiataelCards.add(new Card("mahakam 2", 1, false, 5, "scoiatael"));
+        scoiataelCards.add(new Card("mahakam 3", 1, false, 5, "scoiatael"));
+        scoiataelCards.add(new Card("mahakam 4", 1, false, 5, "scoiatael"));
+        scoiataelCards.add(new Agile("vrihedd brigade", 1, false, 5, "scoiatael"));
+        scoiataelCards.add(new Agile("vrihedd brigade 1", 1, false, 5, "scoiatael"));
+        scoiataelCards.add(new Card("zoltan", 1, false, 5, "neutral"));
+        scoiataelCards.add(new Card("dol archer", 1, false, 4, "scoiatael"));
+        scoiataelCards.add(new Muster("gaunter odimm darkness", 3, false, 4, "neutral"));
+        scoiataelCards.add(new Card("vrihedd cadet", 1, false, 4, "scoiatael"));
+        scoiataelCards.add(new Agile("ciaran", 1, false, 3, "scoiatael"));
+        scoiataelCards.add(new Muster("dwarf", 1, false, 3, "scoiatael"));
+        scoiataelCards.add(new Muster("dwarf 1", 1, false, 3, "scoiatael"));
+        scoiataelCards.add(new Muster("dwarf 2", 1, false, 3, "scoiatael"));
+        scoiataelCards.add(new Horn("dandelion", 1, false, 2, "neutral"));
+        scoiataelCards.add(new Muster("elf skirmisher", 1, false, 2, "scoiatael"));
+        scoiataelCards.add(new Muster("elf skirmisher 1", 1, false, 2, "scoiatael"));
+        scoiataelCards.add(new Muster("elf skirmisher 2", 1, false, 2, "scoiatael"));
+        scoiataelCards.add(new Muster("gaunter odimm", 1, false, 2, "neutral"));
+        scoiataelCards.add(new Card("toruviel", 1, false, 2, "scoiatael"));
+        scoiataelCards.add(new Card("riordain", 1, false, 1, "scoiatael"));
+        scoiataelCards.add(new Cow("cow", 1, false, 0, "neutral"));
+        scoiataelCards.add(new Medic("havekar nurse", 1, false, 0, "scoiatael"));
+        scoiataelCards.add(new Medic("havekar nurse 1", 1, false, 0, "scoiatael"));
+        scoiataelCards.add(new Medic("havekar nurse 2", 1, false, 0, "scoiatael"));
+        scoiataelCards.add(new Spy("mysterious elf", 1, false, 0, "neutral"));
+    }
+    public static ArrayList<Card> getScoiataelCards() {
+        return scoiataelCards;
     }
 }
