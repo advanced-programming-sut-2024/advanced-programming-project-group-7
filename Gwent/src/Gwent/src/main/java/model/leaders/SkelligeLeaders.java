23f464c7baed0cc9b18c14d299a7e4bec028a568
kooroshIM99 <kooroshimani2023@gmail.com>
2024-06-23 12:59:17 +0330

    factionLeaders cards

--- Gwent/src/Gwent/src/main/java/model/leaders/SkelligeLeaders.java
+++ Gwent/src/Gwent/src/main/java/model/leaders/SkelligeLeaders.java
@@ -3,9 +3,16 @@ package model.leaders;
 import model.Leader;
 import model.User;
 
+import java.util.ArrayList;
+
 public class SkelligeLeaders extends Leader {
     public SkelligeLeaders(String leaderName, String description) {
         super(leaderName, description);
+    }
+    private static final ArrayList<Leader> skelligeLeaders=new ArrayList<>();
+    static {
+        skelligeLeaders.add(new SkelligeLeaders("crach an craite","shuffle all cards from each player's graveyard back into their decks"));
+        skelligeLeaders.add(new SkelligeLeaders("king bran","units only lose half their strength in bad weather conditions"));
     }
 
     @Override
