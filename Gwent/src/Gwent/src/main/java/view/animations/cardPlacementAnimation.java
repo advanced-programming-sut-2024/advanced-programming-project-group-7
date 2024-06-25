36b1b944888d102b88eec57696aa1acab18374a6
samyar02 <152595241+samyar02@users.noreply.github.com>
2024-06-25 21:54:54 +0330

    V.1 card animation

--- Gwent/src/Gwent/src/main/java/view/animations/cardPlacementAnimation.java
+++ Gwent/src/Gwent/src/main/java/view/animations/cardPlacementAnimation.java
@@ -1,16 +1,49 @@
 package view.animations;
 
 import javafx.animation.Transition;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.util.Duration;
 import model.Card;
 import model.Game;
+import view.GameLauncher;
 
-public class cardPlacementAnimation extends Transition {
+import java.security.cert.PolicyNode;
 
-    public cardPlacementAnimation (Pane pane, Game game, Card card)
+public class CardPlacementAnimation extends Transition {
+
+    private final int duration = 100;
+    private Card card;
+    private double vy;
+    private double vx;
+    private double endX;
+    private double endY;
+    private Pane pane;
+    private HBox playerRow;
+    private Game game;
+
+    public CardPlacementAnimation(Pane pane, Game game, Card card, HBox row, double vx, double vy, HBox playerRow, double endY, double endX) {
+        this.card = card;
+        this.vx = vx;
+        this.vy = vy;
+        this.game = game;
+        this.pane = pane;
+        this.endX = endX;
+        this.endY = endY;
+        this.setCycleDuration(Duration.millis(duration));
+        this.setCycleCount(-1);
+    }
 
     @Override
     protected void interpolate(double frac) {
-
+        double y = card.getLayoutY() + vy;
+        double x = card.getLayoutX() + vx;
+        if (Math.abs(x - endX) <= 10  && Math.abs(y - endY) <= 10){
+            pane.getChildren().remove(card);
+            game.selectedBox.getChildren().add(card);
+            this.stop();
+        }
+        card.setLayoutX(x);
+        card.setLayoutY(y);
     }
 }
